a1a928f08cc3d8cdf7703c55d3afe7523cffd8b8
Will Haynes <haynes24@gmail.com>
2016-04-28 18:30:44 -0500

    site defines

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -34,8 +34,12 @@ define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 /** badgerherald dev use constants*/
-define('HRLD_DEV', true);
+define('EXA_DEV', true);
 define('HRLD_PRODUCTION', false);
+
+
+define('WP_HOME','http://bhrld.dev/');
+define('WP_SITEURL','http://bhrld.dev/wp/');
 
 /**#@+
  * Authentication Unique Keys and Salts.
